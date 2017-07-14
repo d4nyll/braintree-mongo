4bb4d4deab32c0d5410a57a3f2d2034f76db4045
Daniel Li <dan@danyll.com>
2017-07-14 12:03:18 +0100

    Added more MONGO_URL settings and a MODE setting

--- src/config.js
+++ src/config.js
@@ -1,8 +1,13 @@
 const namespace = 'btm';
-const DEFAULT_MONGO_URL = 'mongodb://localhost:27017/test';
-const MONGO_URL = process.env.MONGO_URL || DEFAULT_MONGO_URL;
+const MODE = 'test';
+const DEFAULT_MONGO_DEV_URL = 'mongodb://localhost:27017/braintree';
+const DEFAULT_MONGO_TEST_URL = 'mongodb://localhost:27017/test';
+const MONGO_DEV_URL = process.env.MONGO_DEV_URL || DEFAULT_MONGO_DEV_URL;
+const MONGO_TEST_URL = process.env.MONGO_TEST_URL || DEFAULT_MONGO_TEST_URL;
+const MONGO_URL = MODE === 'test' ? MONGO_TEST_URL : MONGO_DEV_URL;
+
 export {
   namespace,
-  DEFAULT_MONGO_URL,
+  MODE,
   MONGO_URL,
 }
